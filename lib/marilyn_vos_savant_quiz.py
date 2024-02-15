d07efccf9303982717fd45ba8ef6c02ffa59d47f
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-15 16:41:04 -0700

    Final code. Added one question to m_v_s quiz

--- lib/marilyn_vos_savant_quiz.py
+++ lib/marilyn_vos_savant_quiz.py
@@ -10,7 +10,8 @@ QUESTIONS = {
     "According to the 2014-2015 Australian Bureau of Statistics, what percentage of Australians were born overseas? \u001B[90m(Int & unit)\u001B[0m": ["28%"],
     "What is the weight of a Gold Bar in Fallout: New Vegas? \u001B[90m(Int & unit)\u001B[0m": ["35 Pounds"],
     "What is the romanized Arabic word for 'moon'?": ["Qamar"],
-    "How long did it take the motorized window washers of the original World Trade Center to clean the entire exterior of the building? \u001B[90m(Int & unit)\u001B[0m": ["1 Month"]
+    "How long did it take the motorized window washers of the original World Trade Center to clean the entire exterior of the building? \u001B[90m(Int & unit)\u001B[0m": ["1 Month"],
+    "01010111 01101000 01100001 01110100  01100011 01101111 01101100 01101111 01110010  01100100 01101111 01100101 01110011  01100111 01101111 01101100 01100100  01101100 01100101 01100001 01100110  01100001 01110000 01110000 01100101 01100001 01110010  01101001 01100110  01111001 01101111 01110101  01101000 01101111 01101100 01100100  01101001 01110100  01110101 01110000  01110100 01101111  01110100 01101000 01100101  01101100 01101001 01100111 01101000 01110100 00111111 ?": ["01000111 01110010 01100101 01100101 01101110 "]
 }
 
 def run_marilyn_vos_savant_quiz():
